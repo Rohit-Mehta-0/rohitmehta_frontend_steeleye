ffad884b8d4352f731c604b586745d2c0f4060cc
rohit-mehta-0 <mehtarohit7644@gmail.com>
2023-04-23 21:40:44 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React, { useState, memo } from "react";
+import PropTypes from "prop-types";
+
+// Single List Item
+const WrappedSingleListItem = ({ index, isSelected, onClickHandler, text }) => {
+  return (
+    <li
+      style={{ backgroundColor: isSelected ? "green" : "red" }}
+      onClick={() => onClickHandler(index)}
+    >
+      {text}
+    </li>
+  );
+};
+
+WrappedSingleListItem.propTypes = {
+  index: PropTypes.number,
+  isSelected: PropTypes.bool,
+  onClickHandler: PropTypes.func.isRequired,
+  text: PropTypes.string.isRequired,
+};
+
+const SingleListItem = memo(WrappedSingleListItem);
+
+// List Component
+const WrappedListComponent = ({ items }) => {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const handleClick = (index) => {
+    setSelectedIndex(index);
+  };
+
+  return (
+    <ul style={{ textAlign: "left" }}>
+      {items.map((item, index) => (
+        <SingleListItem
+          key={index}
+          onClickHandler={() => handleClick(index)}
+          text={item.text}
+          index={index}
+          isSelected={selectedIndex === index }
+        />
+      ))}
+    </ul>
+  );
+};
+
+WrappedListComponent.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      text: PropTypes.string.isRequired,
+    })
+  ),
+};
+
+WrappedListComponent.defaultProps = {
+  items: [
+    { text: "Item1", itemId: 1 },
+    { text: "Item2", itemId: 1 },
+    { text: "Item3", itemId: 1 },
+    { text: "Item4", itemId: 1 },
+    { text: "Item5", itemId: 1 },
+    { text: "Item6", itemId: 1 },
+  ],
+};
+
+const List = memo(WrappedListComponent);
+
+export default List;
